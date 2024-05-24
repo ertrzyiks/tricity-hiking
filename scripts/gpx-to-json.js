9a920fbcca1630c8c5cdf8e9540154bacd559ed2
ertrzyiks <mateusz.derks@gmail.com>
2024-05-24 21:30:46 +0200

    Add total loss and gain

--- scripts/gpx-to-json.js
+++ scripts/gpx-to-json.js
@@ -27,30 +27,73 @@ function getDistanceBetweenPoints(lat1, lng1, lat2, lng2) {
   return distance;
 }
 
+const getTotalDistance = (coords) => {
+  const { total: distance } = coords.reduce(
+    (acc, current) => {
+      if (acc.last) {
+        acc.total += getDistanceBetweenPoints(
+          acc.last[1],
+          acc.last[0],
+          current[1],
+          current[0]
+        );
+      }
+
+      acc.last = current;
+      return acc;
+    },
+    { last: null, total: 0 }
+  );
+
+  return distance;
+};
+
+const getTotalGainAndLoss = (coords) => {
+  const { totalGain, totalLoss } = coords.reduce(
+    (acc, current) => {
+      if (acc.last) {
+        const diff = current[2] - acc.last[2];
+        if (diff > 0) {
+          acc.totalGain += diff;
+        } else {
+          acc.totalLoss += Math.abs(diff);
+        }
+      }
+
+      acc.last = current;
+      return acc;
+    },
+    { last: null, totalGain: 0, totalLoss: 0 }
+  );
+
+  return { totalGain, totalLoss };
+};
+
 const process = (json) => {
   json.features.forEach((feature) => {
     if (feature.geometry.type !== "LineString") {
       return;
     }
 
-    const { total: distance } = feature.geometry.coordinates.reduce(
-      (acc, current) => {
-        if (acc.last) {
-          acc.total += getDistanceBetweenPoints(
-            acc.last[1],
-            acc.last[0],
-            current[1],
-            current[0]
-          );
-        }
+    const distance = getTotalDistance(feature.geometry.coordinates);
 
-        acc.last = current;
-        return acc;
-      },
-      { last: null, total: 0 }
+    const { totalGain, totalLoss } = getTotalGainAndLoss(
+      feature.geometry.coordinates
     );
 
     feature.properties.distance = distance;
+    feature.properties.totalGain = totalGain;
+    feature.properties.totalLoss = totalLoss;
+    delete feature.properties.coordinateProperties.times;
+
+    const surfaces = feature.properties.coordinateProperties.Extensionss.map(
+      (surface) => {
+        return surface ? surface.trim() : null;
+      }
+    );
+
+    delete feature.properties.coordinateProperties.Extensionss;
+    feature.properties.coordinateProperties.surfaces = surfaces;
   });
   return json;
 };
