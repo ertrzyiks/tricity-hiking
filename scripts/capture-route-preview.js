13ab58bdda8469892513348db59f0bd1ab41a105
ertrzyiks <mateusz.derks@gmail.com>
2024-07-27 21:24:16 +0200

    Generate previews

---
+++ scripts/capture-route-preview.js
@@ -0,0 +1,39 @@
+import puppeteer from "puppeteer";
+
+const routes = [
+  "dolina-elfow",
+  "dolina-strzyzy",
+  "elblag-bazantarnia-parasol",
+  "lesnik-opera",
+  "orlowo-bulwar",
+  "samborowo-glowica",
+  "sopot-wyscigi-to-kamienny-potok",
+  "wejherowo-kalwaria",
+];
+
+async function captureRoutePreview({ page, route }) {
+  await page.goto(`http://localhost:4321/tricity-hiking/preview/${route}`);
+
+  await page.waitForSelector(".maplibregl-map", { timeout: 30_000 });
+
+  await page.waitForNetworkIdle();
+
+  const element = await page.$(".maplibregl-map");
+  await element.screenshot({
+    path: `./src/assets/routes/${route}.jpg`,
+    type: "jpeg",
+  });
+}
+
+async function run() {
+  const browser = await puppeteer.launch();
+  const page = await browser.newPage();
+
+  for (const route of routes) {
+    await captureRoutePreview({ page, route });
+  }
+
+  browser.close();
+}
+
+run();
---
+++ src/components/PreviewRouteMap/PreviewRouteMap.tsx
@@ -0,0 +1,74 @@
+import { useEffect, useRef } from "preact/hooks";
+import maplibregl from "maplibre-gl";
+import "maplibre-gl/dist/maplibre-gl.css";
+
+import { style } from "../HomeMap/mapStyle";
+import { getBounds } from "../../services/getBounds";
+import pointImage from "../../assets/places/point.png";
+
+export const PreviewRouteMap = ({
+  route,
+}: {
+  route: GeoJSON.FeatureCollection;
+}) => {
+  const mapRef = useRef<HTMLDivElement>(null);
+
+  useEffect(() => {
+    if (mapRef.current === null) return;
+
+    const map = new maplibregl.Map({
+      container: mapRef.current,
+      interactive: false,
+      attributionControl: {
+        compact: false,
+      },
+      style,
+      zoom: 10,
+      minZoom: 9,
+      maxZoom: 15,
+    });
+
+    map.dragRotate.disable();
+    map.touchZoomRotate.disableRotation();
+
+    map.on("load", async () => {
+      const coordinates = route.features.reduce(
+        (acc: [number, number][], feature: any) => {
+          if (feature.geometry.type === "LineString") {
+            return acc.concat(feature.geometry.coordinates);
+          }
+
+          return acc;
+        },
+        []
+      );
+
+      const bounds = getBounds(coordinates);
+
+      map.addSource("lines", {
+        type: "geojson",
+        data: route,
+      });
+
+      map.addLayer({
+        id: "lines",
+        type: "line",
+        source: "lines",
+        paint: {
+          "line-width": 5,
+          "line-color": "#e11d48",
+        },
+      });
+
+      const image = await map.loadImage(pointImage.src);
+      if (!map.hasImage("poi_15")) map.addImage("poi_15", image.data);
+
+      map.fitBounds(bounds, {
+        animate: false,
+        padding: 50,
+      });
+    });
+  }, []);
+
+  return <div ref={mapRef}></div>;
+};
