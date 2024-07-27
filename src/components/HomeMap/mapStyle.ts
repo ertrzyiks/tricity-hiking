9f5071fe6dd67c184b25f4f01e22c86fed65ce11
ertrzyiks <mateusz.derks@gmail.com>
2024-07-27 20:39:15 +0200

    Update maptiler config

--- src/components/HomeMap/mapStyle.ts
+++ src/components/HomeMap/mapStyle.ts
@@ -1,5 +1,6 @@
 export const style =
-  import.meta.env.MODE === "production"
+  import.meta.env.MODE === "production" ||
+  import.meta.env.PUBLIC_ASTRO_USE_MAPTILER_MAP_STYLE
     ? "https://api.maptiler.com/maps/81336bdc-08e3-4a3f-981c-55f06f6e3e42/style.json?key=bHa0L2mKhjZqOJctPuVf"
     : {
         version: 8 as const,
