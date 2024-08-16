9d0c721345ad7d9c16a221286eca7ad111bdc085
ertrzyiks <mateusz.derks@gmail.com>
2024-08-16 20:55:43 +0200

    Add icons

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,9 +3,11 @@ import tailwind from "@astrojs/tailwind";
 import preact from "@astrojs/preact";
 import mdx from "@astrojs/mdx";
 
+import icon from "astro-icon";
+
 // https://astro.build/config
 export default defineConfig({
   site: "https://ertrzyiks.github.io",
   base: "tricity-hiking",
-  integrations: [tailwind(), mdx(), preact()],
+  integrations: [tailwind(), mdx(), preact(), icon()],
 });
