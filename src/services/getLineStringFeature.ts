72242f0947c02a4f51c512a9680b3995d52544ea
ertrzyiks <mateusz.derks@gmail.com>
2024-08-17 20:29:18 +0200

    Improve list

---
+++ src/services/getLineStringFeature.ts
@@ -0,0 +1,15 @@
+export const getLineStringFeature = <
+  Feature extends {
+    geometry: { type: string };
+    properties: Record<string, any>;
+  },
+  FeatureCollection extends { features: Feature[] }
+>({
+  collection,
+}: {
+  collection: FeatureCollection;
+}) => {
+  return collection.features.find(
+    (feature) => feature.geometry.type === "LineString"
+  );
+};
