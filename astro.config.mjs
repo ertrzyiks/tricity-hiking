974e010aee6029077e2392022e5e5886b8922141
ertrzyiks <mateusz.derks@gmail.com>
2025-03-16 20:06:08 +0100

    Add sitemap

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,11 +6,13 @@ import icon from "astro-icon";
 
 import tailwindcss from "@tailwindcss/vite";
 
+import sitemap from "@astrojs/sitemap";
+
 // https://astro.build/config
 export default defineConfig({
   site: "https://ertrzyiks.github.io",
   base: "tricity-hiking",
-  integrations: [mdx(), preact(), icon()],
+  integrations: [mdx(), preact(), icon(), sitemap()],
 
   vite: {
     plugins: [tailwindcss()],
