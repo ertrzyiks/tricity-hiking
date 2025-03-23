78d202842c889a0778d45150599095125c40e49e
ertrzyiks <mateusz.derks@gmail.com>
2025-03-23 21:11:39 +0100

    Add activity links

---
+++ src/content/activities.ts
@@ -0,0 +1,95 @@
+import soldek from "../assets/activities/soldek.png";
+import dar from "../assets/activities/dar-pomorza.png";
+import blyskawica from "../assets/activities/blyskawica.png";
+
+export const ships = [
+  {
+    name: "Sołdek",
+    description: "Steam-powered cargo ship from 1949.",
+    link: "https://nmm.pl/soldek/",
+    location: "Gdańsk",
+    image: soldek,
+  },
+  {
+    name: "Dar Pomorza",
+    description: "Three-masted sailing ship that was used for training.",
+    link: "https://nmm.pl/dar-pomorza/",
+    image: dar,
+    location: "Gdynia",
+  },
+  {
+    name: "Błyskawica",
+    description: "Destroyer, a veteran of the Second World War.",
+    link: "https://muzeummw.pl/orp-blyskawica/",
+    image: blyskawica,
+    location: "Gdynia",
+  },
+];
+
+export const orienteering = [
+  {
+    name: "Harpuś",
+    description:
+      "Checkpoints are validated by a punch card in any order. Stationary only, beginner friendly.",
+    link: "https://www.harpagan.pl/harpus/",
+  },
+  {
+    name: "Parkowe BNO",
+    description:
+      "Checkpoints are validated by an electronic system SportIdent in order provided on the map. Stationary only, beginner friendly.",
+    link: "https://www.parkowebno.pl/",
+  },
+  {
+    name: "Azymut 45 - Z Mapą do Lasu",
+    description:
+      "Stationary and by yourself, terrain and map is more challenging but beginners can still go for the easiest route.",
+    link: "https://www.azymut45gdynia.org/nasze-imprezy/Z-Mapa-do-Lasu/O%20ZMdL",
+  },
+];
+
+export const outdoorPlaygrounds = [
+  {
+    name: "Reagan's Park Playground",
+    description: "",
+    link: "https://sportgdansk.pl/obiekty/kraina-zabawy/",
+  },
+];
+
+export const indoorPlaygrounds = [
+  {
+    name: "Loopy's World",
+    description: "",
+    link: "https://loopys.pl/gdansk/en/",
+    location: "Gdańsk",
+  },
+  {
+    name: "Lider",
+    description: "",
+    link: "https://www.facebook.com/p/Lider-Plac-Zabaw-Gda%C5%84sk-Forum-100064681717408/",
+    location: "Gdańsk",
+  },
+  {
+    name: "Budumi",
+    description: "",
+    link: "https://budumi.pl/",
+    location: "Gdańsk",
+  },
+  {
+    name: "Klockownia",
+    description: "",
+    link: "https://www.klockownia.com/en/",
+    location: "Gdańsk",
+  },
+  {
+    name: "Honolulu",
+    description: "",
+    link: "https://salazabawhonolulu.pl/gdansk/",
+    location: "Gdańsk",
+  },
+  {
+    name: "Amazonia",
+    description: "",
+    link: "https://amazonia.gdynia.pl/en/",
+    location: "Gdynia",
+  },
+];
