5248257ef02af306b2cefa25e24a632f05d00f0a
ertrzyiks <mateusz.derks@gmail.com>
2025-04-22 21:25:08 +0200

    Refactor Route map

---
+++ src/components/RouteMap/createMap.ts
@@ -0,0 +1,27 @@
+import { Map } from "maplibre-gl";
+import { style } from "../HomeMap/mapStyle";
+export const createMap = (
+  container: HTMLElement,
+  initializer: (map: Map) => Promise<() => void>,
+) => {
+  const map = new Map({
+    container: container,
+    style,
+    zoom: 10,
+    minZoom: 9,
+    maxZoom: 15,
+  });
+
+  map.dragRotate.disable();
+  map.touchZoomRotate.disableRotation();
+
+  let cleanUp: (() => void) | void;
+
+  map.on("load", async () => {
+    cleanUp = await initializer(map);
+  });
+
+  return () => {
+    cleanUp?.();
+  };
+};
---
+++ src/components/RouteMap/routePointHighlighter.ts
@@ -0,0 +1,63 @@
+import { type Map } from "maplibre-gl";
+import { $routePoints } from "../../atoms/routePoints";
+import markerImage from "../../assets/places/marker.png";
+
+export const routePointHighlighter = async (
+  map: Map,
+  { coordinates }: { coordinates: number[][] },
+) => {
+  const point = {
+    type: "FeatureCollection" as const,
+    features: [
+      {
+        type: "Feature" as const,
+        properties: {
+          iconSize: 0,
+        },
+        geometry: {
+          type: "Point" as const,
+          coordinates: coordinates[0],
+        },
+      },
+    ],
+  };
+
+  map.addSource("point", {
+    type: "geojson",
+    data: point,
+  });
+
+  const image = await map.loadImage(markerImage.src);
+  if (!map.hasImage("marker_15")) map.addImage("marker_15", image.data);
+
+  map.addLayer({
+    id: "point",
+    source: "point",
+    type: "symbol",
+    layout: {
+      "icon-image": "marker_15",
+      "icon-overlap": "always",
+      "icon-size": ["get", "iconSize"],
+      "icon-allow-overlap": true,
+      "icon-ignore-placement": true,
+      "icon-anchor": "center",
+    },
+  });
+
+  return $routePoints.subscribe((progress) => {
+    if (progress === null) {
+      point.features[0].properties.iconSize = 0;
+    } else {
+      const index = Math.min(
+        coordinates.length - 1,
+        Math.floor(progress * coordinates.length),
+      );
+      const currentCoordinate = coordinates[index];
+      point.features[0].geometry.coordinates = currentCoordinate;
+      point.features[0].properties.iconSize = 1;
+    }
+
+    const source = map.getSource("point") as maplibregl.GeoJSONSource;
+    source.setData(point);
+  });
+};
